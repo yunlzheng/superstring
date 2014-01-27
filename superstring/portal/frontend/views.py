9618a5cea1237274a23cb5e2899d0a2b7b6a5e3e
yunlzheng <yunl.zheng@gmail.com>
2014-01-27 06:28:33 +0000

    删除多余的输入

--- superstring/portal/frontend/views.py
+++ superstring/portal/frontend/views.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 from flask import Blueprint, send_file, send_from_directory
 
-print '/%s' % __name__
 frontend = Blueprint('frontend', __name__, static_folder='static', static_url_path='/statics')
 
 
