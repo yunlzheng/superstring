93039691aa79f233d7744b234ecd431636f6a1b2
yunlzheng <yunl.zheng@gmail.com>
2014-02-25 05:55:48 +0000

    UI原型0.0.1

--- superstring/portal/frontend/static/scripts/controllers/navbar.js
+++ superstring/portal/frontend/static/scripts/controllers/navbar.js
@@ -4,12 +4,35 @@ angular.module('superString')
   .controller('NavbarCtrl', function ($scope, $location) {
     $scope.menu = [{
       'title': '首页',
-      'link': '#/'
+      'link': '#/',
+      'icon': 'fa-home'
     },
     {
-      'title': '帮助',
-      'link': '#/help'
-    }];
+      'title': '计算',
+      'link': '#/compute',
+      'icon': 'fa-tachometer'
+    },
+    {
+        'title': '存储',
+        'link': '#/compute',
+        'icon': 'fa-tasks'
+    },
+    {
+        'title': '网络',
+        'link': '#/compute',
+        'icon': 'fa-cloud'
+    },
+    {
+      'title': '文档',
+      'link': '#/help',
+      'icon': 'fa-star'
+    },
+    {
+      'title': '设置',
+      'link': '#/settings',
+      'icon': 'fa-gear'
+    }
+    ];
     
     $scope.isActive = function(route) {
       return route === '#'+$location.path();
